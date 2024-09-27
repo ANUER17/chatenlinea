d44a071dad21a2d117d89d77d0b2f0d2a4a7979b
ANUER17 <anuerc5@gmail.com>
2024-09-27 11:36:53 -0500

    Update settings.py

--- chatear_amigos/settings.py
+++ chatear_amigos/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'tu-secreta-key-para-django'
 DEBUG = False
 
 # Actualizado con el nombre de tu dominio en Render
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'your-app-name.onrender.com']
+ALLOWED_HOSTS = ['your-app-name.onrender.com', 'localhost', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
