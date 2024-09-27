d6a3b318287922805ba44b13836f9a16b8887db7
ANUER17 <anuerc5@gmail.com>
2024-09-27 12:18:13 -0500

    Update asgi.py

--- chatear_amigos/asgi.py
+++ chatear_amigos/asgi.py
@@ -1,10 +1,14 @@
 import os
+import django
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import chat.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chatear_amigos.settings')
+
+# Asegurarse de que Django esté completamente inicializado antes de configurar websockets
+django.setup()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
